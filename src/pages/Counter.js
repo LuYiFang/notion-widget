136dd6c5fd7c5fa24c22de48665bc7ab772dc0f2
YiFangLu <technology99x@gmail.com>
2024-11-20 18:33:32 +0800

    Fix start date reset

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,11 +1,6 @@
 import _ from "lodash";
 import moment from "moment";
 import { useEffect, useMemo, useState } from "react";
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { IconButton, Popover, Typography } from "@mui/material";
-import SettingsIcon from "@mui/icons-material/Settings";
 import Grid from "@mui/material/Grid2";
 
 const width = 340;
@@ -22,8 +17,12 @@ const numberCardStyle = {
 
 const Counter = () => {
   const [days, setDays] = useState(0);
-  const [startDate, setStartDate] = useState(moment());
-  const [anchorEl, setAnchorEl] = useState(null);
+
+  const startDate = useMemo(() => {
+    const date = new URLSearchParams(window.location.search).get("startDate");
+    if (!date) return moment();
+    return date;
+  }, [window.location.search]);
 
   const { hundreds, tens, units } = useMemo(() => {
     return {
@@ -50,18 +49,6 @@ const Counter = () => {
     return () => clearTimeout(timer);
   }, [days, startDate]);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleDate = (v) => {
-    setStartDate(v);
-  };
-
   return (
     <>
       <Grid
@@ -76,22 +63,6 @@ const Counter = () => {
         rowSpacing={1}
         px={1}
       >
-        <IconButton
-          sx={{
-            position: "absolute",
-            top: 1,
-            right: 8,
-            height: 40,
-            width: 40,
-            color: "transparent",
-            "&:hover": {
-              color: "gray",
-            },
-          }}
-          onClick={handleClick}
-        >
-          <SettingsIcon />
-        </IconButton>
         <Grid container size={12} columnSpacing={1}>
           <Grid sx={numberCardStyle} size={4}>
             {hundreds}
@@ -114,27 +85,6 @@ const Counter = () => {
           Days
         </Grid>
       </Grid>
-      <Popover
-        open={Boolean(anchorEl)}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-      >
-        <LocalizationProvider dateAdapter={AdapterMoment}>
-          <DatePicker
-            value={startDate}
-            onChange={handleDate}
-            format="YYYY/MM/DD"
-          />
-        </LocalizationProvider>
-      </Popover>
     </>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,95 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useMemo, useState } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import Grid from "@mui/material/Grid2";
+import moment from "moment";
+import Counter from "./Counter";
+import {
+  ButtonGroup,
+  Button,
+  TextField,
+  Typography,
+  Tooltip,
+} from "@mui/material";
 
 function Home() {
-    return <>
-        <h1>Notion Widget</h1>
-        <Link to="/counter">Counter</Link>
-    </>;
+  const [startDate, setStartDate] = useState(moment());
+  const [counterUrl, setCounterUrl] = useState("");
+  const [copyMsg, setCopyMsg] = useState("");
+
+  useEffect(() => {
+    setCounterUrl(
+      `${window.location.href}/counter?startDate=${startDate.format(
+        "YYYYMMDD"
+      )}`
+    );
+  }, [startDate]);
+
+  const handleDate = (v) => {
+    setStartDate(v);
+  };
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(counterUrl);
+      setCopyMsg("Copied!");
+    } catch (error) {
+      setCopyMsg("Failed");
+    }
+    setTimeout(handleCopyTooltipClose, 5000);
+  };
+
+  const handleCopyTooltipClose = () => {
+    setCopyMsg("");
+  };
+
+  return (
+    <>
+      <Typography variant="h5">Notion Widget</Typography>
+      <Grid container spacing={1}>
+        <Grid>
+          <Counter />
+        </Grid>
+        <Grid container>
+          <Grid size={12}>
+            <LocalizationProvider dateAdapter={AdapterMoment}>
+              <DatePicker
+                value={startDate}
+                onChange={handleDate}
+                format="YYYY/MM/DD"
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid size={12}>
+            <ButtonGroup>
+              <Tooltip
+                disableFocusListener
+                disableTouchListener
+                disableHoverListener
+                title={copyMsg}
+                open={Boolean(copyMsg)}
+              >
+                <Button onClick={handleCopy}>Copy link</Button>
+              </Tooltip>
+
+              <TextField
+                sx={{
+                  "& .MuiInputBase-root": {
+                    borderRadius: "0 4px 4px 0",
+                  },
+                }}
+                InputProps={{
+                  readOnly: true,
+                }}
+                value={counterUrl}
+              />
+            </ButtonGroup>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default Home;
